ebe4ed3bfa594714809d9ab21506496655df572f
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-21 21:59:37 +0200

    Fetching response and showing it

--- Weatherapp/weatherApi.js
+++ Weatherapp/weatherApi.js
@@ -7,5 +7,15 @@ submitBtn.addEventListener("click", (event) => {
 	const city = document.getElementById("city").value;
 	const apiKey = "your_api_key_here";
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+
+        weatherDiv.innerHTML = `<p>The temperature in ${city}: ${temperature}&degrees;C</p><p>The Current Weather: ${description}</p>`;
+    })
+    .catch((error) => console.log(error));
+
 })
